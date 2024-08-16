4e75a2ed0d0b11d96b32ed22289d859f4d5c7b8f
nagaokakeita <nagakeita12@gmail.com>
2024-08-16 16:23:16 +0900

    chage order

--- init/00_users.sql
+++ init/00_users.sql
@@ -1,11 +1,11 @@
 CREATE TABLE db.users(
-  user_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, 
-  password VARCHAR(30) NOT NULL, 
-  user_name VARCHAR(30) NOT NULL);
+  user_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  user_name VARCHAR(30) NOT NULL,
+  password VARCHAR(30) NOT NULL);
 
 INSERT INTO 
   users
-    (password, user_name) 
+    (user_name, password) 
   VALUES 
-    ("123", "keita"),
-    ("124", "sota");
+    ("keita", "123"),
+    ("sota", "124");
